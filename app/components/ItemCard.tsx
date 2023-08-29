2432fb827e97b90ba97824a3d7cb32c18be396e1
007han <007limhan@gmail.com>
2023-08-29 15:24:57 +0900

    refactor: addItem을 한 계층 위로 이동

--- app/components/ItemCard.tsx
+++ app/components/ItemCard.tsx
@@ -1,45 +1,20 @@
 "use client";
 import "../styles/globals.css";
-import { useRecoilState } from "recoil";
-import { itemState } from "../recoil/itemState";
 import { Menu } from "../type/Menu";
 
-export function MenuCard({ itemName, price }: Menu) {
-  const [itemListState, setItemListState] = useRecoilState(itemState);
-  const addItem = () => {
-    const findMenu = itemListState.find((x) => x.itemName === itemName);
-
-    if (findMenu) {
-      // 이미 한번 선택되었던 메뉴임 추가할것임.
-      setItemListState((prevItemList) =>
-        prevItemList.map((item) =>
-          item.itemName === itemName
-            ? {
-                ...item,
-                itemAmount: (item.itemAmount || 0) + 1,
-                totalPrice: price + item.totalPrice,
-              }
-            : item
-        )
-      );
-    } else {
-      setItemListState((prevItemList) => [
-        //처음 선택되는 아이템 일때 추가하면서 아이템1개로 설정
-        ...prevItemList,
-        {
-          itemName: itemName,
-          itemAmount: 1,
-          totalPrice: price,
-        },
-      ]);
-    }
-  };
-
+/**
+ * 1. 메뉴 데이터를 적절히 렌더링한다
+ * 2. onClick을 적절히 전달한다
+ */
+export function MenuCard({
+  onClick,
+  ...menu
+}: Menu & { onClick: (menu: Menu) => void }) {
   return (
     <div className="itemcardcontainer">
-      <button className="itemcard" onClick={addItem}>
-        <h2>{itemName}</h2>
-        <h2>{price}원</h2>
+      <button className="itemcard" onClick={() => onClick(menu)}>
+        <h2>{menu.itemName}</h2>
+        <h2>{menu.price}원</h2>
       </button>
     </div>
   );
--- app/components/Items.tsx
+++ app/components/Items.tsx
@@ -1,10 +1,14 @@
 "use client";
+import { useRecoilState } from "recoil";
 import { useMenus } from "../hooks/useMenus";
+import { Menu } from "../type/Menu";
 import { MenuCard } from "./ItemCard";
+import { itemState } from "../recoil/itemState";
 
 /**
  * 2. 데이터에서 커피, 논커피 메뉴를 구분한다.
  * 3. 구분된 목록을 각각 순회한다
+ * 4. 메뉴카드를 클릭 시 해당 품목 담기 로직을 적절히 수행한다
  */
 export function Items() {
   const menus = useMenus();
@@ -12,18 +16,48 @@ export function Items() {
   const coffeMenu = menus.filter((menu) => menu.isCoffee);
   const nonCoffeeMenu = menus.filter((menu) => !menu.isCoffee);
 
+  const [itemListState, setItemListState] = useRecoilState(itemState);
+  const addItem = (menu: Menu) => {
+    const findMenu = itemListState.find((x) => x.itemName === menu.itemName);
+
+    if (findMenu) {
+      // 이미 한번 선택되었던 메뉴임 추가할것임.
+      setItemListState((prevItemList) =>
+        prevItemList.map((item) =>
+          item.itemName === menu.itemName
+            ? {
+                ...item,
+                itemAmount: (item.itemAmount || 0) + 1,
+                totalPrice: menu.price + item.totalPrice,
+              }
+            : item
+        )
+      );
+    } else {
+      setItemListState((prevItemList) => [
+        //처음 선택되는 아이템 일때 추가하면서 아이템1개로 설정
+        ...prevItemList,
+        {
+          itemName: menu.itemName,
+          itemAmount: 1,
+          totalPrice: menu.price,
+        },
+      ]);
+    }
+  };
+
   return (
     <div>
       <h2>Coffee</h2>
       <div className="itemcardcontainer">
         {coffeMenu.map((menu) => (
-          <MenuCard key={menu.itemId} {...menu} />
+          <MenuCard key={menu.itemId} {...menu} onClick={addItem} />
         ))}
       </div>
       <h2>non-coffee</h2>
       <div className="itemcardcontainer">
         {nonCoffeeMenu.map((menu) => (
-          <MenuCard key={menu.itemId} {...menu} />
+          <MenuCard key={menu.itemId} {...menu} onClick={addItem} />
         ))}
       </div>
     </div>
